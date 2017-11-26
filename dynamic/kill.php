6eb85923b92d3755b652f028194e331feaed4ff6
Quacky <puzzledog121@gmail.com>
2017-11-25 18:56:13 -0600

    Last fix (hopefully)

--- dynamic/kill.php
+++ dynamic/kill.php
@@ -1,7 +1,7 @@
 <?php
 	function killDead()
 	{
-		$query = "SELECT * FROM servers";
+		$query = "SELECT * FROM servers;";
 		$statement = $GLOBALS['database']->prepare($query);
 		$statement->execute();
 		$result = $statement->fetch(PDO::FETCH_ASSOC);
@@ -9,10 +9,9 @@
 		$deadIp = $result['ip'];
 		$deadPort = $result['port'];
 		$deadUptime = $result['uptime'];
-		$deadMod = $result['mod_'];
 		if ($elapsed > timeout)
 		{
-			$query = "DELETE FROM servers WHERE ip = :deadIp AND port = :deadPort AND uptime = :deadUptime AND mod_ = :deadMod;";
+			$query = "DELETE FROM servers WHERE ip = :deadIp AND port = :deadPort AND uptime = :deadUptime";
 			// We shouldn't just *delete* it because auto increment hates me
 			// Actually forget auto increment delete it anyway
 			//$query = "UPDATE servers SET visible = 0 WHERE ip = :deadIp AND port = :deadPort AND uptime = :deadUptime AND mod_ = :deadMod;";
@@ -20,7 +19,6 @@
 			$statement->bindParam(":deadIp", $deadIp, PDO::PARAM_STR);
 			$statement->bindParam(":deadPort", $deadPort, PDO::PARAM_INT);
 			$statement->bindParam(":deadUptime", $deadUptime, PDO::PARAM_INT);
-			$statement->bindParam(":deadMod", $deadMod, PDO::PARAM_INT);
 			
 			$statement->execute();
 		}
